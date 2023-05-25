e3983fcfa5da38089c8e2d8598facdc4e484ed97
Mai Thi <20130120@st.hcmuaf.edu.vn>
2023-05-25 18:37:08 +0700

    XS đài LamDong->TienGiang

--- app/src/main/java/com/example/kqxs/ListXSTheoDai.java
+++ app/src/main/java/com/example/kqxs/ListXSTheoDai.java
@@ -85,5 +85,86 @@ public class ListXSTheoDai extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+//      XS TIEN GIANG
+        Button btn_tg = findViewById(R.id.kqxstg);
+        btn_tg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListXSTheoDai.this, ShowKQXSTG.class);
+                startActivity(intent);
+            }
+        });
+//        XS SOC TRANG
+        Button btn_soc = findViewById(R.id.kqxssoc);
+        btn_soc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListXSTheoDai.this, ShowKQXSSOC.class);
+                startActivity(intent);
+            }
+        });
+//        xo so quang tri
+        Button btn_qt= findViewById(R.id.kqxsqt);
+        btn_qt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListXSTheoDai.this, ShowKQXSQT.class);
+                startActivity(intent);
+            }
+        });
+//        XO SO QUANG NAM
+        Button btn_qnam= findViewById(R.id.kqxsqnam);
+        btn_qnam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListXSTheoDai.this, ShowKQXSQNAM.class);
+                startActivity(intent);
+            }
+        });
+//      xo so quang ngai
+        Button btn_qn= findViewById(R.id.kqxsqn);
+        btn_qn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListXSTheoDai.this, ShowKQXSQN.class);
+                startActivity(intent);
+            }
+        });
+//        xo số quang binh
+        Button btn_qb= findViewById(R.id.kqxsqb);
+        btn_qb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListXSTheoDai.this, ShowKQXSQB.class);
+                startActivity(intent);
+            }
+        });
+//        xo so phu yen
+        Button btn_py= findViewById(R.id.kqxspy);
+        btn_py.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListXSTheoDai.this, ShowKQXSPY.class);
+                startActivity(intent);
+            }
+        });
+//        xo so ninh thuan
+        Button btn_nt= findViewById(R.id.kqxsnt);
+        btn_nt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListXSTheoDai.this, ShowKQXSNT.class);
+                startActivity(intent);
+            }
+        });
+//        xo so lam dong
+        Button btn_lamdong= findViewById(R.id.kqxslamdong);
+        btn_lamdong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListXSTheoDai.this, ShowKQXSLAMDONG.class);
+                startActivity(intent);
+            }
+        });
     }
 }
